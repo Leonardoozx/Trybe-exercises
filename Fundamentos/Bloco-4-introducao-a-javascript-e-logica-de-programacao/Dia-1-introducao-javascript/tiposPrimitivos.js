01cbd2327af208c2d5bff5e91b2b758c906adcc4
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-03-07 15:48:36 -0300

    Adiciona duas atividades do course

---
+++ Fundamentos/Bloco-4-introducao-a-javascript-e-logica-de-programacao/Dia-1-introducao-javascript/tiposPrimitivos.js
@@ -0,0 +1,9 @@
+const base = 5;
+let height = 8;   
+const area = base * height;
+const resultado = area;
+console.log(resultado);
+
+const perimeter = base*2 + height*2;
+const resultadoPerimetro = perimeter;
+console.log(resultadoPerimetro);
