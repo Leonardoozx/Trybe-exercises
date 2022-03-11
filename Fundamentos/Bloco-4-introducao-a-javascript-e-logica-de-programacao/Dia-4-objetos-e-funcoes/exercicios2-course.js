27193754012d6c479e5a8c1623196390bc721721
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-03-10 21:35:48 -0300

    inica segunda parte das atividades do course mas nao finaliza, porque preciso dormir ;-;

---
+++ Fundamentos/Bloco-4-introducao-a-javascript-e-logica-de-programacao/Dia-4-objetos-e-funcoes/exercicios2-course.js
@@ -0,0 +1,20 @@
+// Exercício 1
+
+console.log('----------------------- Exercício 1 -----------------------');
+
+function palindromo (sameName) {
+    if (sameName.split('').reverse('').join('') == sameName) {
+        return true;
+    }
+    return false;
+}
+
+console.log(palindromo('ana'));
+
+// Exercício 2 
+
+console.log('----------------------- Exercício 2 -----------------------');
+
+function integerArray () {
+
+}
