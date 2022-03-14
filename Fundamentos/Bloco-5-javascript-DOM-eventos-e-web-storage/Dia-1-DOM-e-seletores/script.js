e2454ee100ab213315345851a6d079aa27fe45ee
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-03-14 18:44:07 -0300

    adiciona atividades do course, essa foi mais facil do que aprender a usar o loop for

---
+++ Fundamentos/Bloco-5-javascript-DOM-eventos-e-web-storage/Dia-1-DOM-e-seletores/script.js
@@ -0,0 +1,9 @@
+function changeColors(elements, colors) {
+    document.querySelector(elements).style.backgroundColor = colors;
+
+}
+changeColors('#header-container', 'blue');
+changeColors('.emergency-tasks', 'red');
+changeColors();
+changeColors();
+changeColors();
