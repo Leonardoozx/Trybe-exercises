395035703d9b504b0d199e699854b154811ad9f5
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-04-12 21:32:18 -0300

    .

---
+++ Fundamentos/Bloco-8-higher-order-functions-do-JS-ES6/Dia-4-array-reduce/exercise5.js
@@ -0,0 +1,36 @@
+const names = [
+  'Aanemarie',
+  'Adervandes',
+  'Akifusa',
+  'Abegildo',
+  'Adicellia',
+  'Aladonata',
+  'Abeladerco',
+  'Adieidy',
+  'Alarucha',
+];
+
+// Escreva seu código aqui:
+
+// A forma que eu pensei em fazer usando apenas o for tradicional:
+
+// const namesSliced = names;
+// const aaaa = () => {
+//     let counter = 0;
+//     for (let i = 0; i < namesSliced.length; i += 1) {
+//         const opa = namesSliced[i].split('');
+//         console.log(opa);
+//         for (let i = 0; i<opa.length; i+= 1){
+//             if(opa[i] === 'a' || opa[i] === 'A') {
+//                 counter++;
+//             }
+//         }
+//     }
+//     return counter
+// }
+// console.log(aaaa());
+
+const containsA = (x) => {
+ 
+};
+console.log(containsA(names));
