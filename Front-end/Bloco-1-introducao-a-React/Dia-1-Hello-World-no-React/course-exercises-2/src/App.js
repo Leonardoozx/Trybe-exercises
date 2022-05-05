c1fe8de953bfe5d7b883e611c409458f3c8f6706
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-05-05 20:47:15 -0300

    complete all the exercises from course

---
+++ Front-end/Bloco-1-introducao-a-React/Dia-1-Hello-World-no-React/course-exercises-2/src/App.js
@@ -0,0 +1,18 @@
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import './PapaiChegou.css'
+
+function App() {
+  return (
+    <div className='pai-de-todos'>
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
