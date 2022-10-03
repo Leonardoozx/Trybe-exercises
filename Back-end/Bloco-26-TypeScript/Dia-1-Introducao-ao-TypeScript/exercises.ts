ee68f4fbe6baeb3f3df36989781b79129d2eadde
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-03 20:08:52 -0300

    a

---
+++ Back-end/Bloco-26-TypeScript/Dia-1-Introducao-ao-TypeScript/exercises.ts
@@ -0,0 +1,37 @@
+export function greeter(name: string): string {
+  return `Hello ${name}!`;
+}
+
+export function personAge(name: string, age: number): string {
+  return `${name} is ${age} years old!`;
+}
+
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function sumArray(numbers: number[]): number {
+  return numbers.reduce(add, 0);
+}
+
+export function triangle(base: number, height: number): number {
+  return (base * height) / 2;
+}
+
+export function square(side: number): number {
+  return side ** 2;
+}
+
+export function rectangle(base: number, height: number): number {
+  return base * height;
+}
+
+enum weekDays {
+  monday = 1,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday,
+  sunday
+}
